afa0a116fdf00ee4e68ee728c21733057a310460
Raunak <raunakswain.19402@gmail.com>
2025-07-27 20:05:00 +0530

    E2B-Sandboxes setup

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import {openai, createAgent} from "@inngest/agent-kit";
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },                        // 1. Unique ID of the function
   { event: "test/hello.world" },                // 2. Event that triggers this function
-  async ({ event }) => {                  // 3. Function body (async job)
+  async ({ event, step}) => {                  // 3. Function body (async job)
+
+    const sandboxId = await step.run("get-sandbox-id", async()=>{
+      const sandbox = await Sandbox.create("nova-nextjs-test")
+      return sandbox.sandboxId;
+    })
 
     const codeAgent = createAgent(
    { 
@@ -20,7 +27,13 @@ export const helloWorld = inngest.createFunction(
     const {output} = await codeAgent.run(
       `Write snippets for the following:${event.data.value}`
     )
-    return { output }
+
+    const sandboxUrl = await step.run("get-sandbox-url", async()=>{
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`
+    })
+    return { output , sandboxUrl }
   },
 );
 
